6245ef8e19a0e3e633e958abd45f102a2dc79475
plasticxpower <plasticxpower@gmail.com>
2025-10-06 23:12:31 +0200

    404 avoidance

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+  const base =
+    env.VITE_BASE_PATH && env.VITE_BASE_PATH.trim().length > 0
+      ? env.VITE_BASE_PATH
+      : mode === 'production'
+        ? './'
+        : '/';
   return {
-    base: env.VITE_BASE_PATH || '/',
+    base,
     plugins: [react()],
     build: {
       outDir: 'dist',
